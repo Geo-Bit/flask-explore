80dabb33a404457d1d52448e4f49a4b6d13124ec
George Tipton <gtip22@gmail.com>
2023-01-20 10:51:34 -0700

    Added delete view, fixed bugs, and tested

---
+++ README.md
@@ -0,0 +1,5 @@
+# To Do
+- Improve auth sec (pw reqs, etc.)
+- Improve authorization
+- Implement basic func / non-func test cases
+- Test & fix basic vulns (sqli, etc.)
--- flaskr/blog.py
+++ flaskr/blog.py
@@ -35,13 +35,13 @@ def create():
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO post (title, body, author_id'
+                'INSERT INTO post (title, body, author_id)'
                 ' VALUES (?, ?, ?)',
                 (title, body, g.user['id'])
             )
             db.commit()
             return redirect(url_for('blog.index'))
-    return render_template('blog/creat.html')
+    return render_template('blog/create.html')
 
 def get_post(id, check_author=True):
     post = get_db().execute(
@@ -85,3 +85,12 @@ def update(id):
             return redirect(url_for('blog.index'))
         
     return render_template('blog/update.html', post=post)
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('blog.index'))
